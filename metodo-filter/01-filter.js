a42ffae5252c1a87f89be85604b1dc54d719c581
Dari Alves <darieltondf@gmail.com>
2022-04-15 11:40:58 -0300

    :sunglasses: exercicios filter

--- metodo-filter/01-filter.js
+++ metodo-filter/01-filter.js
@@ -1,45 +1,27 @@
 const animes = [
-    {
-        id: 1,
-        nome: "Dragon Ball",
-        autor: "Akira Toriyama",
-    },
-    {
-        id: 2,
-        nome: "Yu Yu Hakusho",
-        autor: "Yoshihiro Togashi",
-    },
-    {
-        id: 3,
-        nome: "Saint Seiya",
-        autor: "Masami Kurumada",
-    },
-    {
-        id: 4,
-        nome: "Dragon Ball Z",
-        autor: "Akira Toriyama",
-    },
-    {
-        id: 5,
-        nome: "Naruto",
-        autor: "Masashi Kishimoto",
-    },
-
+  {
+    id: 1,
+    nome: 'Dragon Ball',
+    autor: 'Akira Toriyama',
+  },
+  {
+    id: 2,
+    nome: 'Yu Yu Hakusho',
+    autor: 'Yoshihiro Togashi',
+  },
+  {
+    id: 3,
+    nome: 'Saint Seiya',
+    autor: 'Masami Kurumada',
+  },
+  {
+    id: 4,
+    nome: 'Dragon Ball Z',
+    autor: 'Akira Toriyama',
+  },
+  {
+    id: 5,
+    nome: 'Naruto',
+    autor: 'Masashi Kishimoto',
+  },
 ];
-
-// Atividade 1
-
-// for loop 
-
-
-// for of 
-
-
-// filter ()
-
-
-// Atividade 2
-
-
-
-
